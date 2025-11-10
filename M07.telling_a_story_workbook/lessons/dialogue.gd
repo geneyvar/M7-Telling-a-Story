de42738bef2df966f90a165551ec85a8b8fa7970
geneyvar <gelopezbarragan@s.sfusd.edu>
2025-11-09 16:43:58 -0800

    M7L12

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -6,32 +6,60 @@ var expressions := {
 	"sad": preload ("res://assets/emotion_sad.png"),
 }
 
-## An array of dictionaries. Each dictionary has two properties:
+var bodies := {
+	"sophia": preload ("res://assets/sophia.png"),
+	"pink": preload ("res://assets/pink.png")
+}
+
+## Data to display in the dialogue. Each dictionary in this array has three properties:
+##
 ## - expression: a [code]Texture[/code] containing an expression
 ## - text: a [code]String[/code] containing the text the character says
+## - character: a [code]Texture[/code] representing the character
 var dialogue_items: Array[Dictionary] = [
 	{
 		"expression": expressions["regular"],
-		"text": "I'm learning about Arrays...",
-	},
-	{
-		"expression": expressions["sad"],
-		"text": "... and it is a little bit complicated.",
-	},
-	{
-		"expression": expressions["happy"],
-		"text": "Let's see if I got it right: an array is a list of values!",
+		"text": "I've been studying arrays and dictionaries lately.",
+		"character": bodies["sophia"],
 	},
 	{
 		"expression": expressions["regular"],
-		"text": "Did I get it right? Did I?",
+		"text": "Oh, nice. How has it been going?",
+		"character": bodies["pink"],
+	},
+	{
+		"expression": expressions["sad"],
+		"text": "Well... it's a little complicated!",
+		"character": bodies["sophia"],
+	},
+	{
+		"expression": expressions["sad"],
+		"text": "Oh!",
+		"character": bodies["pink"],
+	},
+	{
+		"expression": expressions["regular"],
+		"text": "It sure takes time to click at first.",
+		"character": bodies["pink"],
 	},
 	{
 		"expression": expressions["happy"],
-		"text": "Hehe! Bye bye~!",
+		"text": "If you keep at it, eventually, you'll get the hang of it!",
+		"character": bodies["pink"],
+	},
+	{
+		"expression": expressions["regular"],
+		"text": "Mhhh... I see. I'll keep at it, then.",
+		"character": bodies["sophia"],
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "Thanks for the encouragement. Time to LEARN!!!",
+		"character": bodies["sophia"],
 	},
 ]
 var current_item_index := 0
+
 
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
@@ -45,45 +73,45 @@ func _ready() -> void:
 	next_button.pressed.connect(advance)
 
 
-## Draws the current text to the rich text element
 func show_text() -> void:
-	# We retrieve the current dictionary from the array and assign its
-	# properties to the UI elements.
+	# We first extract data for the current dialogue item and apply it to the UI
+	# elements.
 	var current_item := dialogue_items[current_item_index]
 	rich_text_label.text = current_item["text"]
 	expression.texture = current_item["expression"]
-	# We animate the text appearing letter by letter.
+	body.texture = current_item["character"]
+
+	# We then hide the text and animate it appearing.
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
-	var text_appearing_duration := 1.0
+	var text_appearing_duration: float = current_item["text"].length() / 30.0
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
 
-	# This is where we play the audio. We randomize the audio playback's start
-	# time to make it sound different every time.
+	# We randomize the audio playback's start time to make it sound different
+	# every time and play the sound until the text finishes displaying.
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
 	var sound_start_position := randf() * sound_max_offset
 	audio_stream_player.play(sound_start_position)
-	# We stop the audio when the text finishes appearing.
 	tween.finished.connect(audio_stream_player.stop)
 
+	# We animate the character sliding in.
 	slide_in()
 
-
+## Advances the dialogue to the next item or quits the game if there are no more
+## items.
 func advance() -> void:
 	current_item_index += 1
-	# If we reached the end of the dialogue, we quit the game. Otherwise, we
-	# show the next dialogue line.
 	if current_item_index == dialogue_items.size():
 		get_tree().quit()
 	else:
 		show_text()
 
 
+## Animates the character sliding in.
 func slide_in() -> void:
-	var tween := create_tween()
-	tween.set_trans(Tween.TRANS_QUART)
-	tween.set_ease(Tween.EASE_OUT)
-	body.position.x = 200.0
-	tween.tween_property(body, "position:x", 0.0, 0.3)
-	body.modulate.a = 0.0
-	tween.parallel().tween_property(body, "modulate:a", 1.0, 0.2)
+	var slide_tween := create_tween()
+	slide_tween.set_ease(Tween.EASE_OUT)
+	body.position.x = 200
+	slide_tween.tween_property(body, "position:x", 0, 0.3)
+	body.modulate.a = 0
+	slide_tween.parallel().tween_property(body, "modulate:a", 1, 0.2)
